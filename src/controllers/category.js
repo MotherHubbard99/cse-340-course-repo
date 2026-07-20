f8c64502abf166fdd2502cb22851dafa1c0c2e21
angie <angiewheeler99@gmail.com>
2026-07-20 15:28:35 -0600

    Fix Week03 error for re-submit.

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,15 +1,23 @@
 import { getCategoryById, getProjectsForCategory } from '../models/category.js';
+import { getAllCategories } from '../models/categories.js';
 
-async function categoryDetails(req, res) {
+const showCategoriesPage = async (req, res) => {
+    const categories = await getAllCategories();
+    const title = 'Service Categories';
+
+    res.render('categories', { title, categories });
+};  
+    
+async function category(req, res) {
   const categoryId = req.params.id;
   const category = await getCategoryById(categoryId);
   const projects = await getProjectsForCategory(categoryId);
 
-    res.render("categoryDetails", {
+    res.render("category", {
       title: category.name,
       category,
       projects
     });
 }
 
-export { categoryDetails };
+export { category, showCategoriesPage };
